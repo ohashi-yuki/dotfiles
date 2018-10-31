13d6f2b0389d83adadcbf641a76d0540f4cf1661
大橋勇希 <yuki_ohashi@oohashiisamunozominoMacBook-puro.local>
2018-10-31 15:27:01 +0900

    使用しないライブラリを消した

--- recipe.sh
+++ recipe.sh
@@ -2,7 +2,6 @@ brew cask install dragondrop
 brew cask install dropbox
 brew cask install iterm2
 brew cask install mysqlworkbench
-brew cask install xtrafinder
 brew cask install slack
 brew cask install caffeine
 brew cask install alfred
